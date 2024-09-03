f8c46055d45c6a951debaef18862be24101e4a17
Benedict <benedictumeozor@gmail.com>
2024-09-03 10:01:53 +0100

    refactor: allowed tablet responsiveness

--- components/products/relatedproducts.tsx
+++ components/products/relatedproducts.tsx
@@ -12,7 +12,7 @@ const RelatedProducts = () => {
       <h3 className="mb-8 mt-4 text-xl font-semibold tracking-wide text-dark_grey max-md:text-lg">
         RELATED PRODUCTS
       </h3>
-      <div className="col-span-9 grid w-full grid-cols-2 gap-2 md:grid-cols-4">
+      <div className="col-span-9 grid w-full grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4">
         {products.slice(0, 4).map((product, index) => (
           <Product key={product.id} product={product} index={index} />
         ))}
